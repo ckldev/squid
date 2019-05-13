3e20987de0f78666967c28dddad83c39d8e9df58
ckldev <ckldev@users.noreply.github.com>
2019-05-14 01:01:10 +0800

    Update squid_centos_6.sh

--- squid_centos_6.sh
+++ squid_centos_6.sh
@@ -15,9 +15,9 @@ yum -y install httpd-tools
 
 cd /home/ck
 
-wget http://www.squid-cache.org/Versions/v4/squid-4.7.tar.gz
-tar -zxf squid-4.7.tar.gz
-cd squid-4.7
+wget http://www.squid-cache.org/Versions/v4/squid-3.5.2.tar.gz
+tar -zxf squid-3.5.2.tar.gz
+cd squid-3.5.2
 
 ./configure
 make
