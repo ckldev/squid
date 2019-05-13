d9961a83217b20527f3a9496f59bbeea6e11ed2b
ckldev <ckldev@users.noreply.github.com>
2019-05-14 01:25:38 +0800

    Update squid_centos_6.sh

--- squid_centos_6.sh
+++ squid_centos_6.sh
@@ -27,7 +27,7 @@ htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
 
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 touch /etc/squid/blacklist.acl
-wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/ckldev/squid/master/squid_centos.conf
+wget -O /etc/squid/squid.conf  https://raw.githubusercontent.com/ckldev/squid/master/squid_centos_6.conf
 
 iptables -I INPUT -p tcp --dport 5055 -j ACCEPT
 #/sbin/iptables-save
